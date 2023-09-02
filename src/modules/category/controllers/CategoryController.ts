75be64682ca88d67229176337afa3a897be50632
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-02 01:57:34 -0300

    feat: Adding controller and service for update category

--- src/modules/category/controllers/CategoryController.ts
+++ src/modules/category/controllers/CategoryController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { z } from 'zod'
 import CreateCategoryService from '../services/CreateCategoryService'
 import GetCategoryService from '../services/GetCategoryService'
+import UpdateCategoryService from '../services/UpdateCategoryService'
 
 class CategoryController {
   async create(req: Request, res: Response) {
@@ -24,6 +25,23 @@ class CategoryController {
     const { id } = CategorySchema.parse(req.params)
 
     const category = await GetCategoryService.execute({ id })
+
+    return res.status(200).json(category)
+  }
+
+  async update(req: Request, res: Response) {
+    const FindSchema = z.object({
+      id: z.string().cuid(),
+    })
+
+    const CategorySchema = z.object({
+      name: z.string().optional(),
+    })
+
+    const { id } = FindSchema.parse(req.params)
+    const { name } = CategorySchema.parse(req.body)
+
+    const category = await UpdateCategoryService.execute({ id, name })
 
     return res.status(200).json(category)
   }
