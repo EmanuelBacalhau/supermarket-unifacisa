835e26e2e16ba30e0bf8b7e707fd120a81132b73
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-02 17:05:11 -0300

    feat: Adding controller and service for update product

--- src/modules/product/controllers/ProductController.ts
+++ src/modules/product/controllers/ProductController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { z } from 'zod'
 import CreateProductService from '../services/CreateProductService'
 import GetProductService from '../services/GetProductService'
+import UpdateProductService from '../services/UpdateProductService'
 
 class ProductController {
   async create(req: Request, res: Response) {
@@ -36,6 +37,34 @@ class ProductController {
     const { id } = FindSchema.parse(req.params)
 
     const product = await GetProductService.execute({ id })
+
+    return res.status(200).json(product)
+  }
+
+  async update(req: Request, res: Response) {
+    const FindSchema = z.object({
+      id: z.string().cuid(),
+    })
+    const ProductSchema = z.object({
+      name: z.string().optional(),
+      description: z.string().optional(),
+      price: z.number().nonnegative().optional(),
+      categoryId: z.string().cuid().optional(),
+      barCode: z.string().optional(),
+      manufacturingDate: z
+        .string()
+        .transform((element) => new Date(element))
+        .optional(),
+      expirationDate: z
+        .string()
+        .transform((element) => new Date(element))
+        .optional(),
+    })
+
+    const { id } = FindSchema.parse(req.params)
+    const data = ProductSchema.parse(req.body)
+
+    const product = await UpdateProductService.execute({ id, ...data })
 
     return res.status(200).json(product)
   }
