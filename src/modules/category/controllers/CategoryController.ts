07396ab24e19e9e8c81d0cd9ac0e2bbe5e5a3d90
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-02 01:37:56 -0300

    feat: Adding controller and services for get category

--- src/modules/category/controllers/CategoryController.ts
+++ src/modules/category/controllers/CategoryController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { z } from 'zod'
 import CreateCategoryService from '../services/CreateCategoryService'
+import GetCategoryService from '../services/GetCategoryService'
 
 class CategoryController {
   async create(req: Request, res: Response) {
@@ -13,6 +14,18 @@ class CategoryController {
     const category = await CreateCategoryService.execute({ name })
 
     return res.status(201).json(category)
+  }
+
+  async show(req: Request, res: Response) {
+    const CategorySchema = z.object({
+      id: z.string().cuid(),
+    })
+
+    const { id } = CategorySchema.parse(req.params)
+
+    const category = await GetCategoryService.execute({ id })
+
+    return res.status(200).json(category)
   }
 }
 
