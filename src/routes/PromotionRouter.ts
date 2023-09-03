527933032b770aac70fe1eca551489c133ec7fd9
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-02 23:36:38 -0300

    feat: Adding router for create promotion (POST /api/promotions/register)

---
+++ src/routes/PromotionRouter.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+import PromotionController from '../modules/promotion/controllers/PromotionController'
+
+class PromotionRouter {
+  private router: Router
+
+  constructor() {
+    this.router = Router()
+    this.setup()
+  }
+
+  private setup() {
+    this.router.post('/promotions/register', PromotionController.create)
+  }
+
+  public get getRouter() {
+    return this.router
+  }
+}
+
+export default new PromotionRouter()
