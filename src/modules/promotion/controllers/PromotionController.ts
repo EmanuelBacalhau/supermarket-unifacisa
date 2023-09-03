0006c4ce1f1dfc32d576be24884cae5c7857f931
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-03 00:01:13 -0300

    feat: Adding controller and service for update promotion

--- src/modules/promotion/controllers/PromotionController.ts
+++ src/modules/promotion/controllers/PromotionController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { z } from 'zod'
 import CreatePromotionService from '../services/CreatePromotionService'
 import GetPromotionService from '../services/GetPromotionService'
+import UpdatePromotionService from '../services/UpdatePromotionService'
 
 class PromotionController {
   async create(req: Request, res: Response) {
@@ -33,6 +34,34 @@ class PromotionController {
     const { id } = FindSchema.parse(req.params)
 
     const promotion = await GetPromotionService.execute({ id })
+
+    return res.status(200).json(promotion)
+  }
+
+  async update(req: Request, res: Response) {
+    const PromotionSchema = z.object({
+      name: z.string().optional(),
+      percentage: z.number().nonnegative().optional(),
+      startDate: z
+        .string()
+        .nonempty()
+        .transform((element) => new Date(element))
+        .optional(),
+      endDate: z
+        .string()
+        .nonempty()
+        .transform((element) => new Date(element))
+        .optional(),
+    })
+
+    const FindSchema = z.object({
+      id: z.string(),
+    })
+
+    const data = PromotionSchema.parse(req.body)
+    const { id } = FindSchema.parse(req.params)
+
+    const promotion = await UpdatePromotionService.execute({ id, ...data })
 
     return res.status(200).json(promotion)
   }
