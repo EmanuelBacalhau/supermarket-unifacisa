bdc6264e53f82d13eb30d57c9bc0fdde237dcb07
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-08 22:31:42 -0300

    feat: add router, controller and service fot orderDetails

--- src/routes/OrderRouter.ts
+++ src/routes/OrderRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import finishAndCreateOrderOrderController from '../modules/order/controllers/finishAndCreateOrder-order.controller'
+import orderDetailsController from '../modules/order/controllers/order-details.controller'
 
 class OrderRouter {
   private router: Router
@@ -10,6 +11,7 @@ class OrderRouter {
   }
 
   private setup() {
+    this.router.get('/orders/:orderId/details', orderDetailsController.handle)
     this.router.put(
       '/orders/finish/:orderId',
       finishAndCreateOrderOrderController.handle,
